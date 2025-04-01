21b0fe56105352987f54db9fc622a1db7690a488
Sander De Puydt <pxldemo@gmail.com>
2025-04-01 18:41:11 +0200

    final solution

--- ADO.NET-disconnected-3---SteamSpringSale/SteamLibraryClassLibrary/SteamGamesData.cs
+++ ADO.NET-disconnected-3---SteamSpringSale/SteamLibraryClassLibrary/SteamGamesData.cs
@@ -1,5 +1,6 @@
 ﻿using System.Data;
 using System.Globalization;
+using System.Runtime;
 using System.Text;
 using System.Xml;
 
@@ -15,11 +16,10 @@ namespace SteamLibraryClassLibrary
 
         private static DataSet _steamDataSet;
         public static DataSet SteamDataSet { get { return _steamDataSet; } }
+        public static DataTable SteamGamesDataTable { get { return _steamDataSet.Tables["Games"]; } }
 
-        public static void ImportFromCsv(string fileName)
+        static SteamGamesData()
         {
-            string csvFilePath = fileName;
-
             // Maak de DataTables
             _gamesTable = CreateGamesDataTable();
             _categoriesTable = CreateCategoriesDataTable();
@@ -33,10 +33,13 @@ namespace SteamLibraryClassLibrary
             _steamDataSet.Tables.Add(_genresTable);
             _steamDataSet.Tables.Add(_gameCategoriesTable);
             _steamDataSet.Tables.Add(_gameGenresTable);
+        }
 
-
+        public static void ImportFromCsv(string fileName)
+        {
+            string csvFilePath = fileName;
             // Lees het CSV-bestand en vul de DataTables
-            ReadCsvAndPopulateDataTables(csvFilePath, _gamesTable, _categoriesTable, _genresTable, _gameCategoriesTable, _gameGenresTable);
+            ReadCsvAndPopulateDataTables(csvFilePath);
         }
 
         private static DataTable CreateGamesDataTable()
@@ -59,14 +62,14 @@ namespace SteamLibraryClassLibrary
         private static DataTable CreateCategoriesDataTable()
         {
             DataTable table = new DataTable("Categories");
-            table.Columns.Add("category_name", typeof(string));
+            table.Columns.Add("name", typeof(string));
             return table;
         }
 
         private static DataTable CreateGenresDataTable()
         {
             DataTable table = new DataTable("Genres");
-            table.Columns.Add("genre_name", typeof(string));
+            table.Columns.Add("name", typeof(string));
             return table;
         }
 
@@ -86,7 +89,7 @@ namespace SteamLibraryClassLibrary
             return table;
         }
 
-        private static void ReadCsvAndPopulateDataTables(string csvFilePath, DataTable gamesTable, DataTable categoriesTable, DataTable genresTable, DataTable gameCategoriesTable, DataTable gameGenresTable)
+        private static void ReadCsvAndPopulateDataTables(string csvFilePath)
         {
             using (StreamReader sr = new StreamReader(csvFilePath))
             {
@@ -108,7 +111,7 @@ namespace SteamLibraryClassLibrary
                     bool isFree = columns[19] == "WAAR";
                     decimal priceInitial = decimal.Parse(columns[20]);
 
-                    DataRow gameRow = gamesTable.NewRow();
+                    DataRow gameRow = _gamesTable.NewRow();
                     gameRow["steam_appid"] = steamAppId;
                     gameRow["name"] = CheckForUnknown(name);
                     gameRow["total_reviews"] = totalReviews;
@@ -126,46 +129,53 @@ namespace SteamLibraryClassLibrary
                     }
                     gameRow["is_free"] = isFree;
                     gameRow["price_initial"] = priceInitial;
-                    gamesTable.Rows.Add(gameRow);
+                    _gamesTable.Rows.Add(gameRow);
 
                     // Voeg categorieën toe (studenten best aanmoedigen om hiervoor een functie te schrijven als dit lukt)
                     string[] categories = columns[4].Replace("['", "").Replace("']", "").Split("', '");
-                    foreach (var category in categories)
-                    {
-                        if (!IsValueInTable(categoriesTable, "category_name", category))
-                        {
-                            DataRow categoryRow = categoriesTable.NewRow();
-                            categoryRow["category_name"] = category;
-                            categoriesTable.Rows.Add(categoryRow);
-                        }
-
-                        // Voeg de game-categorie koppeling toe
-                        DataRow gameCategoryRow = gameCategoriesTable.NewRow();
-                        gameCategoryRow["steam_appid"] = steamAppId;
-                        gameCategoryRow["category_name"] = category;
-                        gameCategoriesTable.Rows.Add(gameCategoryRow);
-                    }
+                    AddCategoryRelationForSteamAppId(categories, steamAppId);
 
                     // Voeg de genres toe (studenten aanmoedigen om hiervoor een functie te schrijven)
                     string[] genres = columns[5].Replace("['", "").Replace("']", "").Split(new[] { "', '" }, StringSplitOptions.None);
-                    foreach (var genre in genres)
-                    {
-                        if (!IsValueInTable(genresTable, "genre_name", genre))
-                        {
-                            DataRow genreRow = genresTable.NewRow();
-                            genreRow["genre_name"] = genre;
-                            genresTable.Rows.Add(genreRow);
-                        }
-
-                        // Voeg de game-genre koppeling toe
-                        DataRow gameGenreRow = gameGenresTable.NewRow();
-                        gameGenreRow["steam_appid"] = steamAppId;
-                        gameGenreRow["genre_name"] = genre;
-                        gameGenresTable.Rows.Add(gameGenreRow);
-                    }
+                    AddGenreRelationForSteamAppId(genres, steamAppId);
                 }
             }
+        }
 
+        private static void AddCategoryRelationForSteamAppId(string[] categories, int steamAppId)
+        {
+            foreach (var category in categories)
+            {
+                if (!IsValueInTable(_categoriesTable, "name", category))
+                {
+                    DataRow categoryRow = _categoriesTable.NewRow();
+                    categoryRow["name"] = category;
+                    _categoriesTable.Rows.Add(categoryRow);
+                }
+                // Voeg de game-genre koppeling toe
+                DataRow gameCategoryRow = _gameCategoriesTable.NewRow();
+                gameCategoryRow["steam_appid"] = steamAppId;
+                gameCategoryRow["category_name"] = category;
+                _gameCategoriesTable.Rows.Add(gameCategoryRow);
+            }
+        }
+
+        private static void AddGenreRelationForSteamAppId(string[] genres, int steamAppId)
+        {
+            foreach (var genre in genres)
+            {
+                if (!IsValueInTable(_genresTable, "name", genre))
+                {
+                    DataRow genreRow = _genresTable.NewRow();
+                    genreRow["name"] = genre;
+                    _genresTable.Rows.Add(genreRow);
+                }
+                // Voeg de game-genre koppeling toe
+                DataRow gameGenreRow = _gameGenresTable.NewRow();
+                gameGenreRow["steam_appid"] = steamAppId;
+                gameGenreRow["genre_name"] = genre;
+                _gameGenresTable.Rows.Add(gameGenreRow);
+            }
         }
 
         private static bool IsValueInTable(DataTable dt, string columnName, string value)
@@ -191,11 +201,10 @@ namespace SteamLibraryClassLibrary
             return value;
         }
 
-        public static DataTable SearchGamesTable(string searchTerm)
+        public static DataTable SearchGamesTable(string searchTerm, DataTable dataTableToFilter)
         {
-            DataTable filteredGamesTable = _gamesTable.Clone();
-
-            foreach (DataRow row in _gamesTable.Rows)
+            DataTable filteredGamesTable = dataTableToFilter.Clone();
+            foreach (DataRow row in dataTableToFilter.Rows)
             {
                 string gameName = row["name"].ToString();
                 if (gameName.Contains(searchTerm, StringComparison.OrdinalIgnoreCase))
@@ -203,9 +212,103 @@ namespace SteamLibraryClassLibrary
                     filteredGamesTable.ImportRow(row);
                 }
             }
+            return filteredGamesTable;
+        }
+
+        public static List<string> GetAllGenres()
+        {
+            List<string> genres = new List<string>();
+
+            foreach (DataRow row in _genresTable.Rows)
+            {
+                genres.Add(row["name"].ToString());
+            }
+            return genres;
+        }
+
+        public static List<string> GetAllCategories()
+        {
+            List<string> categories = new List<string>();
+
+            foreach (DataRow row in _categoriesTable.Rows)
+            {
+                categories.Add(row["name"].ToString());
+            }
+            return categories;
+        }
+
+        public static List<string?> GetAllGenresBySteamAppId(int steamAppId)
+        {
+            return _gameGenresTable.AsEnumerable().Where(x => Convert.ToInt32(x["steam_appid"]) == steamAppId).
+                Select(x => x["genre_name"].ToString()).ToList();
+        }
+        public static List<string?> GetAllCategoriesBySteamAppId(int steamAppId)
+        {
+            return _gameCategoriesTable.AsEnumerable().Where(x => Convert.ToInt32(x["steam_appid"]) == steamAppId).
+                Select(x => x["category_name"].ToString()).ToList(); ;
+        }
+
+        public static DataTable GetAllProfitableGames()
+        {
+            DataTable profitableGames = _gamesTable.Copy();
+            profitableGames.Columns.Add("profit", typeof(double));
+            foreach (DataRow row in profitableGames.Rows)
+            {
+                row["profit"] = Convert.ToDouble(row["price_initial"]) / 100 * Convert.ToInt32(row["total_reviews"]);
+            }
+            profitableGames.Columns.Remove("total_reviews");
+            profitableGames.Columns.Remove("total_positive");
+            profitableGames.Columns.Remove("total_negative");
+            profitableGames.Columns.Remove("review_score");
+            profitableGames.Columns.Remove("is_released");
+            profitableGames.Columns.Remove("release_date");
+            profitableGames.Columns.Remove("is_free");
+            return profitableGames;
+        }
+
+        public static DataTable GetAllPopularGames()
+        {
+            DataTable popularGames = _gamesTable.Copy();
+            popularGames.Columns.Add("popularity_score", typeof(double));
+            for (int i = 0; i < popularGames.Rows.Count; i++)
+            {
+                DataRow row = popularGames.Rows[i];
+                row["popularity_score"] = Convert.ToDouble(row["total_positive"]) / Convert.ToDouble(row["total_reviews"]);
+                if (Convert.ToInt32(row["total_positive"]) <= 5000 || Convert.ToDouble(row["popularity_score"]) < 0.9 )
+                {
+                    popularGames.Rows.RemoveAt(i);
+                    i--;
+                }
+            }
+            popularGames.Columns.Remove("is_released");
+            popularGames.Columns.Remove("release_date");
+            popularGames.Columns.Remove("is_free");
+            popularGames.Columns.Remove("price_initial");
+            return popularGames;
+        }
+
+        public static DataTable FilterGenre(string genre, DataTable dataTableToFilter)
+        {
+            DataTable filteredGamesTable = dataTableToFilter.Clone();
+            var filteredRows = dataTableToFilter.AsEnumerable().Join(
+                _gameGenresTable.AsEnumerable().
+                Where(x => x["genre_name"].ToString().Equals(genre)),
+                x => x["steam_appid"], x => x["steam_appid"], (x, y) => x);
+            filteredRows.ToList().ForEach(x => filteredGamesTable.ImportRow(x));
 
             return filteredGamesTable;
         }
 
+        public static DataTable FilterCategories(string category, DataTable dataTableToFilter)
+        {
+            DataTable filteredGamesTable = dataTableToFilter.Clone();
+            var filteredRows = dataTableToFilter.AsEnumerable().Join(
+                _gameCategoriesTable.AsEnumerable().
+                Where(x => x["category_name"].ToString().Equals(category)),
+                x => x["steam_appid"], x => x["steam_appid"], (x, y) => x);
+            filteredRows.ToList().ForEach(x => filteredGamesTable.ImportRow(x));
+
+            return filteredGamesTable;
+        }
     }
 }
